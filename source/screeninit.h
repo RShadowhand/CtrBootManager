28bf61ae7b988c7d9334a26a88a67221408e6c8c
OperationNT414C <alexandre.lia.414c@gmail.com>
2016-05-08 12:42:08 +0200

    Create screeninit.h

---
+++ source/screeninit.h
@@ -0,0 +1,15 @@
+/*
+*   screeninit.h
+*
+*   Screen init code by dark_samus, bil1s, Normmatt, delebile and others.
+*   Screen deinit code by tiniVi.
+*/
+
+#pragma once
+
+#include "arm9/source/common.h"
+
+#define PDN_GPU_CNT        (*(vu8 *)0x10141200)
+
+void deinitScreens(void);
+void initScreens(void);
