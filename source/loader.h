07e22e6c53e9b027bbda7380e3531661b165ad9d
OperationNT414C <OperationNT414C@users.noreply.github.com>
2016-05-21 17:36:16 +0200

    Update loader.h

--- source/loader.h
+++ source/loader.h
@@ -1,12 +1,14 @@
 #ifndef _loader_h_
 #define _loader_h_
 
-int load(char *path, long offset);
+typedef struct {
+    char memToSearch[256];
+    char memOverwrite[256];
+    int memToSearchSize;
+    int memOverwriteSize;
+    int occurence;
+} binary_patch;
 
-int load_bin(char *path, long offset);
-
-#ifndef ARM9
-int load_3dsx(char *path);
-#endif
+int load(char *path, long offset, binary_patch* patches, int patchesCount);
 
 #endif // _loader_h_
