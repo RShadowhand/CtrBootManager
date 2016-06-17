2eeb3b556cff5957d0562aa6920667e33980745c
OperationNT414C <OperationNT414C@users.noreply.github.com>
2016-06-17 21:28:42 +0200

    Update movie.c

--- source/movie.c
+++ source/movie.c
@@ -24,31 +24,41 @@ int readMovieFrame(void* iFileHandlePtr, int* ioFileOffset, comp_manager_s* iCom
 
         readBytes = fileHandleRead(iFileHandlePtr, iCompMgr->frame_comp + 9, comp_size - 9, *ioFileOffset); // Read compressed buffer
 
-        if (readBytes != (comp_size - 9) || qlz_size_decompressed(iCompMgr->frame_comp) != iFrameBufferSize) // Check if EOF
+        if (readBytes != (comp_size - 9) || readCompressedMovieFrameFromBuffer(iCompMgr, oFrameBuffer, iFrameBufferSize)) // Check if EOF
             return -1; // Disable the animation
+
         *ioFileOffset += readBytes;
-
-        qlz_decompress(iCompMgr->frame_comp, iCompMgr->frame_curr, &iCompMgr->state); // Decompress the frame
-
-        for (size_t i = 0; i < iFrameBufferSize; i++) // Perform delta decoding
-            iCompMgr->frame_curr[i] += iCompMgr->frame_prev[i];
-
-        memcpy(iCompMgr->frame_prev, iCompMgr->frame_curr, iFrameBufferSize); // Keep a copy of the fb
-        memcpy(oFrameBuffer, iCompMgr->frame_curr, iFrameBufferSize); // Display it on the screen
         return 0;
     }
 }
 
-int getMovieFramesCount(void* iFileHandlePtr, int iFrameBufferSize, int iCompressed, int* ioFrameOffset)
+int readCompressedMovieFrameFromBuffer(comp_manager_s* iCompMgr, char* oFrameBuffer, int iFrameBufferSize)
+{
+    if ( qlz_size_decompressed(iCompMgr->frame_comp) != iFrameBufferSize )
+        return -1;
+    
+    qlz_decompress(iCompMgr->frame_comp, oFrameBuffer, &iCompMgr->state); // Decompress the frame
+
+    if ( iCompMgr->reverse )
+    {
+        for (size_t i = 0; i < iFrameBufferSize; i++) // Perform delta decoding
+            oFrameBuffer[i] = iCompMgr->frame_prev_read[i] - oFrameBuffer[i];
+    }
+    else
+    {
+        for (size_t i = 0; i < iFrameBufferSize; i++) // Perform delta decoding
+            oFrameBuffer[i] += iCompMgr->frame_prev_read[i];
+    }
+
+    memcpy(iCompMgr->frame_prev_write, oFrameBuffer, iFrameBufferSize); // Keep a copy of the fb
+    return 0;
+}
+
+int getMovieFramesCount(void* iFileHandlePtr, int iFrameBufferSize, int iCompressed)
 {
     size_t fileSize = fileHandleSize(iFileHandlePtr);
     if ( !iCompressed )
-    {
-        int framesCount = fileSize / iFrameBufferSize;
-        if ( NULL != ioFrameOffset )
-            *ioFrameOffset = ( *ioFrameOffset < framesCount && *ioFrameOffset >=0 ) ? (*ioFrameOffset * iFrameBufferSize) : -1;
-        return framesCount;
-    }
+        return fileSize / iFrameBufferSize;
     else
     {
         char compHeader[9];
@@ -60,18 +70,47 @@ int getMovieFramesCount(void* iFileHandlePtr, int iFrameBufferSize, int iCompres
             size_t comp_size = qlz_size_compressed(compHeader); // Get compressed size
             if ( fileOffset + comp_size > fileSize )
                 break;
-            if ( NULL != ioFrameOffset && *ioFrameOffset == framesCount )
-            {
-                *ioFrameOffset = fileOffset;
-                ioFrameOffset = NULL;
-            }
             fileOffset += comp_size;
             framesCount++;
         }
-        
-        if ( NULL != ioFrameOffset )
-            *ioFrameOffset = -1; // We didn't find any valid frame
-        
+
         return framesCount;
+    }
+}
+
+void getMovieDataOffsetForFrames(void* iFileHandlePtr, int iFrameBufferSize, int iCompressed, int iStartFrame, int iEndFrame,
+                                 int* oStartFrameOffset, int* oDataSizeToEndFrame)
+{
+    size_t fileSize = fileHandleSize(iFileHandlePtr);
+    if ( !iCompressed )
+    {
+        int framesCount = fileSize / iFrameBufferSize;
+        if ( NULL != oStartFrameOffset )
+            *oStartFrameOffset = iFrameBufferSize * ( ( iStartFrame < framesCount ) ? iStartFrame : (framesCount-1) );
+        if ( NULL != oDataSizeToEndFrame )
+            *oDataSizeToEndFrame = iFrameBufferSize * ( ( iEndFrame < framesCount ) ? iEndFrame : (framesCount-1) );
+    }
+    else
+    {
+        char compHeader[9];
+        int curFrame = 0;
+        int fileOffset = 0;
+        int firstFrameOffset = -1;
+
+        while ( curFrame <= iEndFrame && 9 == fileHandleRead(iFileHandlePtr, compHeader, 9, fileOffset) )
+        {
+            size_t comp_size = qlz_size_compressed(compHeader); // Get compressed size
+            if ( fileOffset + comp_size > fileSize )
+                break;
+            if ( curFrame == iStartFrame )
+                firstFrameOffset = fileOffset;
+            fileOffset += comp_size;
+            curFrame++;
+        }
+
+        if ( NULL != oStartFrameOffset )
+            *oStartFrameOffset = firstFrameOffset;
+        if ( NULL != oDataSizeToEndFrame )
+            *oDataSizeToEndFrame = fileOffset;
     }
 }
